afba266974b509147066c6c08dbb928d025aff37
Franekskc <franek5432@gmail.com>
2024-05-07 16:49:10 +0200

    set const lenght of a song

--- songs/song_creator.py
+++ songs/song_creator.py
@@ -1,8 +1,12 @@
 def create_song(text: str) -> str:
     """Converts song in string for hex ascii values"""
+    lenght = 300
     song = ""
-    for symbol in text:
-        song += f"{hex(ord(symbol))[2:]:02}"
+    for i in range(0,lenght):
+        if i < len(text):
+            song += f"{hex(ord(text[i]))[2:]:02}"
+        else:
+            song += f"{hex(ord(' '))[2:]:02}"
     return song
 
 print(create_song("        f khh gffu hhggf fkhhggffs a     ff khh gffu hhggf fkhhggffg s    fsfffsfffsffffffhfhhku h gg g g  f h f f saf f sa  gk gl gk gl gk u h g g fgf s fffff  fhfgss as hf fsff fsff f h fhf fsff fsff f h fh k uhgg f  ffgfhg g f ghs"))
